e7730585da01dd711cf1bd014b6e14c37830a81e
enppp <94575200+enppp@users.noreply.github.com>
2026-03-02 08:45:04 -0600

    Create Dialouge 1.js

---
+++ Chapter 1/Story/Dialouge 1.js
@@ -0,0 +1,12 @@
+// functions are inorder on how the dialouge is added
+
+function createDialouge1() {
+  
+  let textContainer = document.getElementById("container1")
+  let head1 = document.createElement("h1")
+
+  textContainer.appendChild(head1)
+
+  head1.innerHTML = ""
+
+}
